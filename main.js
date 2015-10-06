2330d7df37714666ac8087cdd94a4cf787267070
sethdaniel <sethmcdonaldcode@gmail.com>
2015-10-06 10:38:27 -0400

    added color chooser

--- main.js
+++ main.js
@@ -20,8 +20,32 @@ function initialize(){
 //DESCRIPTION : bondage is a storage area for all DOM elements that need to be bound or re-bound.  It should be called during initialization or whenever new elements are injected into the DOM and need to be bound.
 function bondage(){
    
-      
    //CLICK LISTENERS
+   
+   //SAVE NOTE BUTTON
+   $('#save_note_pseudobutton').on('click', function(){
+      
+      
+      
+   });
+   
+   //NEW NOTE BUTTON
+   $('#new_note_pseudobutton').on('click', function(){
+      
+      //create a new note object
+      var newNoteObj = new NoteObj();
+      
+      //get html form string
+      var newNoteFormHtml = newNoteObj.generateEditForm();
+      
+      //inject the form html into the DOM
+      $('#notes_container').html(newNoteFormHtml);
+      
+   });
+   
+   
+   
+   
    
    //CHANGE LISTENERS
 
@@ -89,6 +113,37 @@ function NoteObj(){
    this.color = '';
    this.positionTop = 0;
    this.positionLeft = 0;
+   
+   //methods
+   this.generateEditForm = function(){
+      
+      var editFormHtml = '<input type="hidden" id="note_id_field" value="' + this.id +'">\n\
+                          <div id="title_container">\n\
+                           <span id="title_label" class="form_label">Title:</span>\n\
+                           <span id="title_input"><input type="text" id="title_textbox" value="' + this.title + '"></span>\n\
+                          </div>\n\
+                          <hr>\n\
+                          <div id="content_container">\n\
+                           <span id="content_label" class="form_label">Note:</span>\n\
+                           <span id="content_input"><textarea id="content_textbox" rows="30" cols="400">' + this.content + '</textarea></span>\n\
+                          </div>\n\
+                          <div id="color_chooser_container">\n\
+                           <select id="color_chooser">\n\
+                              <option id="red_option" class="color_option" value="rgba(255, 0, 0, 0.7)">red</option>\n\
+                              <option id="orange_option" class="color_option" value="rgba(255, 127, 0, 0.7)">orange</option>\n\
+                              <option id="yellow_option" class="color_option" value="rgba(255, 255, 0, 0.7)">yellow</option>\n\
+                              <option id="green_option" class="color_option" value="rgba(0, 255, 0, 0.7)">green</option>\n\
+                              <option id="blue_option" class="color_option" value="rgba(0, 0, 255, 0.7)">blue</option>\n\
+                              <option id="purple_option" class="color_option" value="rgba(75, 0, 130, 0.7)">purple</option>\n\
+                           </select>\n\
+                          </div>\n\
+                          <div id="notes_buttons_container">\n\
+                           <div id="save_note_pseudobutton" class="pseudobutton">Save Note</div> \n\
+                          </div>';
+      
+      return editFormHtml;
+      
+   }
    
 }
 
